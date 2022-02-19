00081035b8fc6dde9810578cebb1a49c8901e5ab
Soeren Boll Overgaard <soren@overgaard.org>
2022-02-19 15:46:11 +0100

    Fix number of loaded configuration chunks, now that another config file was added

--- test/test_configuration.py
+++ test/test_configuration.py
@@ -13,9 +13,9 @@ class TestConfiguration(unittest.TestCase):
 
     def test_read_configuration_dir(self):
         c = configuration.Configuration('test/data/config')
-        self.assertEqual(len(c.workers()), 2)
-        self.assertEqual(len(c.worker_sets()), 1)
-        self.assertEqual(len(c.scps()), 1)
+        self.assertEqual(len(c.workers()), 4)
+        self.assertEqual(len(c.worker_sets()), 2)
+        self.assertEqual(len(c.scps()), 2)
         self.assertIsNotNone(c.core())
 
 if __name__ == "__main__":
