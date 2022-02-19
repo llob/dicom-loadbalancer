d3f0dbbf5b54918d03a7b85bb5c7e897e9423f8a
Soeren Boll Overgaard <soren@overgaard.org>
2022-02-19 13:46:30 +0100

    Handle updated default configuration file in tests

--- test/test_configuration.py
+++ test/test_configuration.py
@@ -4,7 +4,7 @@ import configuration
 class TestConfiguration(unittest.TestCase):
     def test_read_configuration_file(self):
         c = configuration.Configuration('test/data/config/sample-config.json')
-        self.assertEqual(len(c.workers()), 1)
+        self.assertEqual(len(c.workers()), 2)
         self.assertEqual(len(c.worker_sets()), 1)
         self.assertEqual(len(c.worker_sets()[0].header_requirements), 1)
         self.assertEqual(len(c.scps()), 1)
@@ -13,10 +13,10 @@ class TestConfiguration(unittest.TestCase):
 
     def test_read_configuration_dir(self):
         c = configuration.Configuration('test/data/config')
-        self.assertEqual(len(c.workers()), 1)
+        self.assertEqual(len(c.workers()), 2)
         self.assertEqual(len(c.worker_sets()), 1)
         self.assertEqual(len(c.scps()), 1)
         self.assertIsNotNone(c.core())
 
-
-#unittest.main()
+if __name__ == "__main__":
+    unittest.main()
